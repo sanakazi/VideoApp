49968a4178135c929ed398776960d66fae1ecda0
SanaKazi <kazisana23@gmail.com>
2017-11-01 13:36:00 +0530

    original

--- app/src/main/java/com/example/sanakazi/videoapp/VideoControlsActivity.java
+++ app/src/main/java/com/example/sanakazi/videoapp/VideoControlsActivity.java
@@ -1,5 +1,6 @@
 package com.example.sanakazi.videoapp;
 
+import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
@@ -21,10 +22,12 @@ import android.widget.TextView;
 
 import java.io.IOException;
 
+import static com.example.sanakazi.videoapp.R.id.rlPlayerView;
+
 public class VideoControlsActivity extends AppCompatActivity implements SurfaceHolder.Callback {
 
     private static final String TAG = VideoControlsActivity.class.getSimpleName();
-    RelativeLayout rlBottomControls;
+    RelativeLayout rlBottomControls,frameContainer,rlPlayerView;
     private SurfaceView mSurfaceView;
     private MediaPlayer mMediaPlayer;
     public static SurfaceHolder mSurfaceHolder;
@@ -32,12 +35,13 @@ public class VideoControlsActivity extends AppCompatActivity implements SurfaceH
     private ImageView ivPlay;
     public static boolean isPlaying = true;
     public static SeekBar seekBarVideo;
-    public static TextView tvDuration,tvStartTime;
+    public static TextView tvDuration, tvStartTime;
+    public static int store_seek_progress = 0;
 
 
-     private static final String VIDEO_PATH="http://classicmusic-9521.kxcdn.com/video/Tu%20hi%20hai%20aashiqui%20orignal%20song%20from%20movie.mp4";
-   // private static final String VIDEO_PATH = "https://inducesmile.com/wp-content/uploads/2016/05/small.mp4";
-  //  private static final String VIDEO_PATH ="http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4";
+    //  private static final String VIDEO_PATH="http://classicmusic-9aaa.kxcdn.com/video/Tu%20hi%20hai%20aashiqui%20orignal%20song%20from%20movie.mp4";
+    // private static final String VIDEO_PATH = "https://inducesmile.com/wp-content/uploads/2016/05/small.mp4";
+    private static final String VIDEO_PATH = "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4";
     private static int bottomPlayerVisibility = 1;
     private static int SPLASH_TIME_OUT = 3000;
 
@@ -51,7 +55,7 @@ public class VideoControlsActivity extends AppCompatActivity implements SurfaceH
         }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_controls);
-        Log.w(TAG,"onCreate() of activity");
+        Log.w(TAG, "onCreate() of activity");
         initialize();
         click_events();
 
@@ -59,16 +63,19 @@ public class VideoControlsActivity extends AppCompatActivity implements SurfaceH
 
 
     private void initialize() {
+        rlPlayerView= (RelativeLayout) findViewById(R.id.rlPlayerView);
+        frameContainer = (RelativeLayout) findViewById(R.id.frameContainer);
         rlBottomControls = (RelativeLayout) findViewById(R.id.rlBottomControls);
         mSurfaceView = (SurfaceView) findViewById(R.id.surface_view);
         ivPlay = (ImageView) findViewById(R.id.ivPlay);
-        tvDuration=(TextView)findViewById(R.id.tvDuration);
-        tvStartTime=(TextView)findViewById(R.id.tvStartTime);
-        seekBarVideo=(SeekBar)findViewById(R.id.seekBar) ;
+        tvDuration = (TextView) findViewById(R.id.tvDuration);
+        tvStartTime = (TextView) findViewById(R.id.tvStartTime);
+        seekBarVideo = (SeekBar) findViewById(R.id.seekBar);
         tvStartTime.setText("00:00:00");
         tvDuration.setText("00:00:00");
         mSurfaceHolder = mSurfaceView.getHolder();
         mSurfaceHolder.addCallback(VideoControlsActivity.this);
+
     }
 
 
@@ -90,7 +97,7 @@ public class VideoControlsActivity extends AppCompatActivity implements SurfaceH
                     ivPlay.setImageResource(R.mipmap.stop_video);
 
                 } else {
-                   // videoStream_object.play(VIDEO_PATH);
+                    // videoStream_object.play(VIDEO_PATH);
                     ivPlay.setImageResource(R.mipmap.play_video);
                     videoStream_object.playerResume();
 
@@ -103,19 +110,27 @@ public class VideoControlsActivity extends AppCompatActivity implements SurfaceH
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
-        Log.w(TAG, "surface surfaceCreated" + ", isPlaying = "+ isPlaying);
+        Log.w(TAG, "surface surfaceCreated" + ", isPlaying = " + isPlaying);
+        if (videoStream_object == null) {
+            if (isPlaying) {
+                videoStream_object = new VideoStream(VideoControlsActivity.this);
+                videoStream_object.play(VIDEO_PATH);
+                videoStream_object.setSeekBar(seekBarVideo);
 
-        if(isPlaying) {
-            videoStream_object = new VideoStream(VideoControlsActivity.this);
-            videoStream_object.play(VIDEO_PATH);
+            }
         }
-        videoStream_object.setSeekBar((SeekBar)findViewById(R.id.seekBar));
+        else
+        {
+            videoStream_object.playerForSurfaceView();
+        }
+
 
     }
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
         Log.w(TAG, "surface surfaceChanged");
+
 
     }
 
@@ -127,7 +142,7 @@ public class VideoControlsActivity extends AppCompatActivity implements SurfaceH
     @Override
     protected void onPause() {
         super.onPause();
-        Log.w(TAG,"onPause() of activity");
+        Log.w(TAG, "onPause() of activity");
         ivPlay.setImageResource(R.mipmap.stop_video);
         videoStream_object.pause();
 
@@ -136,33 +151,66 @@ public class VideoControlsActivity extends AppCompatActivity implements SurfaceH
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.w(TAG,"onDestroy() of activity");
+        Log.w(TAG, "onDestroy() of activity");
         videoStream_object.releaseMediaPlayer();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.w(TAG,"onResume() of activity"+ ",isPlaying = " + isPlaying);
+        Log.w(TAG, "onResume() of activity" + ",isPlaying = " + isPlaying);
         if (isPlaying) {
             ivPlay.setImageResource(R.mipmap.play_video);
         } else {
             ivPlay.setImageResource(R.mipmap.stop_video);
-           // videoStream_object.playerResume();
+            // videoStream_object.playerResume();
         }
 
 
     }
 
-/* public void onConfigurationChanged(Configuration newConfig) {
+ public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
 
         // Checks the orientation of the screen
         if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
           Log.w(TAG,"Configuration is " + " landscape" + newConfig.orientation);
+
+
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            rlBottomControls.setVisibility(View.VISIBLE);
+
+           // player.setUpVideoDimensions();
+            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+            frameContainer.setLayoutParams(params);
+            RelativeLayout.LayoutParams paramsBottom = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+            paramsBottom.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, rlPlayerView.getId());
+            rlBottomControls.setLayoutParams(paramsBottom);
+
+
+            //this code will return access to automatic sensor again for orientation
+            new Handler().postDelayed(new Runnable() {
+
+                @Override
+                public void run() {
+
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
+
+                }
+            }, 2000);
+
+
+
+
+
+
+
+
         } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
             Log.w(TAG,"Configuration is " + " portrait" );
         }
-    }*/
+    }
+
+
 }
 
